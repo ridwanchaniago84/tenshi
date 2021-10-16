4780d5775f2254b8ce18c74297a0f3251866f66c
ridwanchaniago84 <ridwanchaniago84@gmail.com>
2021-10-16 14:04:00 +0700

    Complete send magnet torrent

--- src/Screen/Torrrent/Index.js
+++ src/Screen/Torrrent/Index.js
@@ -4,32 +4,50 @@ import {
     StyleSheet,
     Text,
     TextInput,
-    Button
+    Button,
+    TouchableHighlight,
+    Linking,
+    ToastAndroid
 } from 'react-native';
-import IntentLauncher, { IntentConstant } from 'react-native-intent-launcher'
-import RNInstalledApplication from 'react-native-installed-application';
+// import IntentLauncher, { IntentConstant } from 'react-native-intent-launcher'
+// import RNInstalledApplication from 'react-native-installed-application';
 
 
 
-const openOtherApp = () => {
-    RNInstalledApplication.getApps()
-        .then(apps => {
-            let obj = apps.find(o => o.appName === 'example Wallpapers');
-            if (!obj) return console.log('success');
-            console.log(obj)
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    // IntentLauncher.startAppByPackageName('io.stellio.player')
-    //     .then((result) => {
-    //         console.log('startAppByPackageName started');
-    //     })
-    //     .catch((error) => console.warn('startAppByPackageName: could not open', error));
-}
+// const openOtherApp = () => {
+//     // RNInstalledApplication.getApps()
+//     //     .then(apps => {
+//     //         let obj = apps.find(o => o.appName === 'example Wallpapers');
+//     //         if (!obj) return console.log('success');
+//     //         console.log(obj)
+//     //     })
+//     //     .catch(error => {
+//     //         console.log(error);
+//     //     }); // Listing App
+//     // IntentLauncher.startAppByPackageName('io.stellio.player')
+//     //     .then((result) => {
+//     //         console.log('startAppByPackageName started');
+//     //     })
+//     //     .catch((error) => console.warn('startAppByPackageName: could not open', error)); // Open App
+// }
 
 const Torrent = () => {
     const [borderColor, setBorderColor] = useState('#0000004d');
+    const [magnet, setMagnet] = useState('');
+
+    const sendMagnet = () => {
+        fetch('https://ztorrentdowloader.herokuapp.com/add-magnet', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                magnet: magnet
+            })
+        })
+        .then(() => ToastAndroid.show("Magnet Sended", ToastAndroid.SHORT))
+    }
 
     return (
         <View style={styles.container}>
@@ -46,6 +64,8 @@ const Torrent = () => {
                     }}
                     onFocus={() => setBorderColor('#00aff4')}
                     onBlur={() => setBorderColor('#0000004d')}
+                    value={magnet}
+                    onChangeText={setMagnet}
                 />
                 <View style={styles.buttonContainer}>
                     <Button
@@ -53,8 +73,13 @@ const Torrent = () => {
                         title="Download"
                         color="#5865f2"
                         accessibilityLabel="Learn more about this purple button"
-                        onPress={() => openOtherApp()}
+                        onPress={sendMagnet}
                     />
+                </View>
+                <View style={{ marginTop: 15 }}>
+                    <TouchableHighlight onPress={() => { Linking.openURL('https://ztorrentdowloader.herokuapp.com/file/') }}>
+                        <Text style={{ color: '#1484b2', fontSize: 15 }}>View File</Text>
+                    </TouchableHighlight>
                 </View>
             </View>
         </View>
